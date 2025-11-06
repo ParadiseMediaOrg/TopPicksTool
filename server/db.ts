42f582c9cbf1d6ef5876671465c9ed0eaab0e223
mscerri-pm <malcolm.scerri@paradisemedia.com>
2025-11-06 12:41:21 +0100

    Import pg and redefine Pool for database connection

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,7 @@
-import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
